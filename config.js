8f15539d1695661265e8f6c3b71e30cb664c53bc
richardfillan <102147467+richardfillan@users.noreply.github.com>
2025-06-19 16:00:20 +0200

    manual add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration file for Asset Management App
+// Replace these values with your actual credentials
+
+window.CONFIG = {
+    API_KEY: 'AIzaSyD813c1sltnUtRi8XnArS8Emz3x2VrH0QU',
+    CLIENT_ID: '276342216052-b5hvo9u20t2qhtp36a82t7ag9gt5kfj2.apps.googleusercontent.com',
+    SHEET_ID: '1DaccKEJC_fSLdyBipOGbmeQ-RUmuUVMrjPLsHIFKmaM',
+    DISCOVERY_DOC: 'https://sheets.googleapis.com/$discovery/rest?version=v4',
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets'
+};
